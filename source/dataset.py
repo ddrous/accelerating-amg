e720e1c80381e25ed09382ff5b92de6378904f31
RDN WIN11 <desmond.ngueguin@gmail.com>
2022-07-01 16:20:39 +0100

    Copied Luz code

--- source/dataset.py
+++ source/dataset.py
@@ -1,13 +1,32 @@
-import pyamg
+from sklearn.utils import shuffle
 
 
-size = 2
-def create_dataset(size=10, dist='lognormal'):
-    if dist=='poisson':
-        A = pyamg.gallery.poisson((size, size), dtype='float32', type='FE')
-    elif dist=='lognormal':
-        A = generate_lognormal(())
-    return A
+class DataSet:
+    def __init__(self, As, Ss, coarse_nodes_list, baseline_P_list):
+        self.As = As
+        self.Ss = Ss
+        self.coarse_nodes_list = coarse_nodes_list
+        self.baseline_P_list = baseline_P_list
 
-A = create_dataset(dist='poisson')
-print(A.todense())
+    def shuffle(self):
+        As, Ss, coarse_nodes_list, baseline_P_list = shuffle(self.As,
+                                                             self.Ss,
+                                                             self.coarse_nodes_list,
+                                                             self.baseline_P_list)
+        return DataSet(As, Ss, coarse_nodes_list, baseline_P_list)
+
+    def __getitem__(self, item):
+        return DataSet(
+            self.As[item],
+            self.Ss[item],
+            self.coarse_nodes_list[item],
+            self.baseline_P_list[item]
+        )
+
+    def __add__(self, other):
+        return DataSet(
+            self.As + other.As,
+            self.Ss + other.Ss,
+            self.coarse_nodes_list + other.coarse_nodes_list,
+            self.baseline_P_list + other.baseline_P_list
+        )
